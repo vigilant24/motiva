03c98230888201bc7b46aa9194dd66b2c2864255
vigilant24 <38216911+vigilant24@users.noreply.github.com>
2024-01-14 00:51:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const slider = document.querySelector('.slider');
 function activate(e) {
   const items = document.querySelectorAll('.item');
   e.target.matches('.next') && slider.append(items[0]);
-  e.target.matches('.prev') && slider.append(items[items.ngth -1]);
+  e.target.matches('.prev') && slider.append(items[items.nth -1]);
 }
 document.addEventListener('click',activate,false);
